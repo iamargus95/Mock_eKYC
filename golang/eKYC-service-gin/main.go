dc4288b1281d7f15c01d4a10f3fc69a72e6fb6f6
iamargus95 <kamathsuraj95@gmail.com>
2021-09-16 13:31:44 +0530

    Added handler to Add a new item (POST)

--- golang/eKYC-service-gin/main.go
+++ golang/eKYC-service-gin/main.go
@@ -19,9 +19,9 @@ type album struct {
 //albums slice to seed record albm data
 
 var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{"1", "Blue Train", "John Coltrane", 56.99},
+	{"2", "Jeru", "Gerry Mulligan", 17.99},
+	{"3", "Sarah Vaughan and Clifford Brown", "Sarah Vaughan", 39.99},
 }
 
 //getAlbums responds with the list of all albums as JSON.
@@ -33,6 +33,20 @@ func getAlbums(c *gin.Context) {
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	//call Bind JSON to bind the received JSON to newAlbum
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
