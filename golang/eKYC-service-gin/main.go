48e373fee88e5868f9709d0f0d00de406089482a
iamargus95 <kamathsuraj95@gmail.com>
2021-09-16 12:41:17 +0530

    Initial Commit

---
+++ golang/eKYC-service-gin/main.go
@@ -0,0 +1,38 @@
+package main
+
+//Tutorial available at https://golang.org/doc/tutorial/web-service-gin
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+//Struct of data
+
+type album struct {
+	ID     string  `json"id"`
+	Title  string  `json"title"`
+	Artist string  `json"artist"`
+	Price  float64 `json"price"`
+}
+
+//albums slice to seed record albm data
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+//getAlbums responds with the list of all albums as JSON.
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
+}
