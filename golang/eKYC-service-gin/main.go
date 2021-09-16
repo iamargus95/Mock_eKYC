2500b820e32d5501168f11933804474d64043df7
iamargus95 <kamathsuraj95@gmail.com>
2021-09-16 13:50:27 +0530

    Added handler to search using ID

--- golang/eKYC-service-gin/main.go
+++ golang/eKYC-service-gin/main.go
@@ -10,18 +10,18 @@ import (
 //Struct of data
 
 type album struct {
-	ID     string  `json"id"`
-	Title  string  `json"title"`
-	Artist string  `json"artist"`
-	Price  float64 `json"price"`
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
 }
 
 //albums slice to seed record albm data
 
 var albums = []album{
-	{"1", "Blue Train", "John Coltrane", 56.99},
-	{"2", "Jeru", "Gerry Mulligan", 17.99},
-	{"3", "Sarah Vaughan and Clifford Brown", "Sarah Vaughan", 39.99},
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
 //getAlbums responds with the list of all albums as JSON.
@@ -33,11 +33,13 @@ func getAlbums(c *gin.Context) {
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumById)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
 
+//Add an entry to the Database
 func postAlbums(c *gin.Context) {
 	var newAlbum album
 
@@ -49,4 +51,20 @@ func postAlbums(c *gin.Context) {
 
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+//Get data by id field
+
+func getAlbumById(c *gin.Context) {
+	id := c.Param("id")
+
+	//Loop over the list of albums, looking for an album whose ID values match the parameter.
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Album not found."})
 }
