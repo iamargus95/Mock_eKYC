496778a9a0ea95764f22cf73b730e65f4ef442e2
iamargus95 <kamathsuraj95@gmail.com>
2021-09-22 13:54:24 +0530

    Added Plan string to response data struct

--- golang/eKYC-service-gin/resources/api/v1/clients.go
+++ golang/eKYC-service-gin/resources/api/v1/clients.go
@@ -4,4 +4,5 @@ type ClientsResponse struct {
 	ID    uint   `json:"id"`
 	Name  string `json:"name,omitempty"`
 	Email string `json:"email,omitempty" validate:"required,email"`
+	Plan  string `json:"required"`
 }
