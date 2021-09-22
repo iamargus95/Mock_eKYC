39510c2f1c74554eaada656eb4f4d8ba433e406a
iamargus95 <kamathsuraj95@gmail.com>
2021-09-22 13:54:02 +0530

    Improved database modelling by using gorm.Model in place of base model

--- golang/eKYC-service-gin/models/clients.go
+++ golang/eKYC-service-gin/models/clients.go
@@ -1,52 +1,27 @@
 package models
 
-import (
-	"log"
-
-	"github.com/jinzhu/gorm"
-)
+import "github.com/jinzhu/gorm"
 
 //Clients Structure.
-type Clients struct {
-	Model
-
+type Client struct {
+	gorm.Model
 	Name  string `gorm:"type:varchar(50)" json:"name" validate:"required"`
 	Email string `gorm:"type:varchar(50)" json:"email" validate:"required,email"`
-	Plans Plans
+	Plan  Plan
 }
 
-type Plans struct {
-	Model
-
+//Plans Structure.
+type Plan struct {
+	gorm.Model
 	ClientID uint
-	Plan     string
+	Plan     string `gorm:"type:varchar(10)" json:"plan" validate:"required"`
 }
 
 //TableName return name of database table
-func (d *Clients) TableName() string {
+func (d *Client) TableName() string {
 	return "clients"
 }
 
-func (d *Plans) TableName() string {
+func (d *Plan) TableName() string {
 	return "plans"
-}
-
-func CreateClientsTable(db *gorm.DB) error {
-
-	dbInfo := db.CreateTable(&Clients{})
-	if dbInfo == nil {
-		log.Fatalf("Error while creating Clients table.")
-	}
-	log.Printf("Clients table created.")
-	return nil
-}
-
-func CreatePlansTable(db *gorm.DB) error {
-
-	dbInfo := db.CreateTable(&Plans{})
-	if dbInfo == nil {
-		log.Fatalf("Error while creating Plans table.")
-	}
-	log.Printf("Plans table created.")
-	return nil
 }
