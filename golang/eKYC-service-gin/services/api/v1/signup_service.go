c3071b34abc391dbe7997ae40d2ffeba2dcd7944
iamargus95 <kamathsuraj95@gmail.com>
2021-09-25 06:50:47 +0530

    Cleaned up signup_service

--- golang/eKYC-service-gin/services/api/v1/signup_service.go
+++ golang/eKYC-service-gin/services/api/v1/signup_service.go
@@ -1,42 +1,16 @@
 package v1service
 
 import (
-	u "iamargus95/eKYC-service-gin/apiHelpers"
 	"iamargus95/eKYC-service-gin/conn"
 	"iamargus95/eKYC-service-gin/models"
-	v1r "iamargus95/eKYC-service-gin/resources/api/v1"
+	"log"
 )
 
-type SignupService interface {
-	Save(v1r.Request) map[string]interface{}
-}
-
-type signupService struct {
-	clients []v1r.Request
-}
-
-func New() SignupService {
-	return &signupService{}
-}
-
-func (service *signupService) FindAll() map[string]interface{} {
-	db := conn.GetDB()
-	db.Find(&service.clients)
-	resp := u.Message(200, "Success")
-	return resp
-}
-
-func (service *signupService) Save(body v1r.Request) map[string]interface{} {
-	//DB needed
-	//Include ORM here.
-	//Pass entire struct as arg
+func NewClient(clients models.Client) {
 
 	db := conn.GetDB()
-	db.Create(&models.Client{
-		Name:  body.Name,
-		Email: body.Email,
-		Plan:  models.Plan{Plan: body.Plan},
-	})
-	resp := u.Message(200, "Success")
-	return resp
+	err := db.Create(&clients)
+	if err.Error != nil {
+		log.Fatalf("POST operation failed.")
+	}
 }
