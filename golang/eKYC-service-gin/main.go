f4753e4fb04bcb2bc1920eb16a9de945ef8931fe
iamargus95 <kamathsuraj95@gmail.com>
2021-09-25 06:42:38 +0530

    Added create if not present for tables

--- golang/eKYC-service-gin/main.go
+++ golang/eKYC-service-gin/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"iamargus95/eKYC-service-gin/conn"
+	"iamargus95/eKYC-service-gin/models"
 	"iamargus95/eKYC-service-gin/routes"
+	"log"
 )
 
 func main() {
 
+	db := conn.GetDB()
+	err1 := db.DropTableIfExists(&models.Plan{}, &models.Client{})
+	if err1.Error == nil {
+		err2 := db.CreateTable(&models.Client{}, &models.Plan{})
+		if err2.Error != nil {
+			log.Fatalf("Table creation failed.")
+		}
+	}
 	routes.StartGin()
+	defer db.Close()
 }
