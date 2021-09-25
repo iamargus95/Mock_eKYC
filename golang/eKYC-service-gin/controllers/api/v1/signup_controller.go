f0e6347fd13bc709c67005f6f4eb1175eb976c2c
iamargus95 <kamathsuraj95@gmail.com>
2021-09-25 06:43:07 +0530

    changed var type

--- golang/eKYC-service-gin/controllers/api/v1/signup_controller.go
+++ golang/eKYC-service-gin/controllers/api/v1/signup_controller.go
@@ -1,29 +1,17 @@
 package controllers
 
 import (
-	v1resources "iamargus95/eKYC-service-gin/resources/api/v1"
+	"iamargus95/eKYC-service-gin/models"
 	v1s "iamargus95/eKYC-service-gin/services/api/v1"
 
 	"github.com/gin-gonic/gin"
 )
 
-type SignupController interface {
-	Save(ctx *gin.Context)
-}
+func NewClient(ctx *gin.Context) {
 
-type controller struct {
-	service v1s.SignupService
-}
-
-func New(service v1s.SignupService) SignupController {
-	return &controller{
-		service: service,
+	var body models.Client
+	err := ctx.BindJSON(&body)
+	if err == nil {
+		v1s.NewClient(body)
 	}
-}
-
-func (c *controller) Save(ctx *gin.Context) {
-
-	var body v1resources.Request
-	ctx.BindJSON(&body)
-	c.service.Save(body)
 }
