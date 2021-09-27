d09d92d13061f3fa27d0eb8cb35c0e6cbd0fa346
iamargus95 <kamathsuraj95@gmail.com>
2021-09-27 15:33:47 +0530

    Added Field Specific validation errors

--- golang/eKYC-service-gin/controllers/api/v1/client_controller.go
+++ golang/eKYC-service-gin/controllers/api/v1/client_controller.go
@@ -3,7 +3,7 @@ package controllers
 import (
 	v1r "iamargus95/eKYC-service-gin/resources/api/v1"
 	v1s "iamargus95/eKYC-service-gin/services/api/v1"
-	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,9 +13,15 @@ func Signup(ctx *gin.Context) {
 	var body v1r.Request
 	err := ctx.BindJSON(&body)
 	if err != nil {
-		log.Fatal(err)
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"errorMessage": err.Error(),
+		})
 	} else {
 		v1s.Signup(body)
+		ctx.JSON(http.StatusOK, gin.H{
+			"accessKey": "10-char-JWT-Token",
+			"secretKey": "20-char-JWT-Token",
+		})
 	}
 
 }
