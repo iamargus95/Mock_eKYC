1a62a3dccccf572d9eed564799042a39427ea9e1
iamargus95 <kamathsuraj95@gmail.com>
2021-09-27 11:31:51 +0530

    Added AutoMigrate to create tables

--- golang/eKYC-service-gin/main.go
+++ golang/eKYC-service-gin/main.go
@@ -4,19 +4,12 @@ import (
 	"iamargus95/eKYC-service-gin/conn"
 	"iamargus95/eKYC-service-gin/models"
 	"iamargus95/eKYC-service-gin/routes"
-	"log"
 )
 
 func main() {
 
 	db := conn.GetDB()
-	err1 := db.DropTableIfExists(&models.Plan{}, &models.Client{})
-	if err1.Error == nil {
-		err2 := db.CreateTable(&models.Client{}, &models.Plan{})
-		if err2.Error != nil {
-			log.Fatalf("Table creation failed.")
-		}
-	}
+	db.Debug().AutoMigrate(&models.Client{}, &models.Plan{})
 	routes.StartGin()
 	defer db.Close()
 }
