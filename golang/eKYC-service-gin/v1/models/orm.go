9860649c776f3184e1b09f9162f85386918dcc9e
iamargus95 <kamathsuraj95@gmail.com>
2021-09-30 12:23:02 +0530

    Added a model for secrets table cleaned up unwanted fields from plans table

--- golang/eKYC-service-gin/v1/models/orm.go
+++ golang/eKYC-service-gin/v1/models/orm.go
@@ -2,19 +2,21 @@ package models
 
 import "gorm.io/gorm"
 
+//Structs for api/v1/signup
 //Clients Structure.
 type Client struct {
 	gorm.Model
-	Name  string `gorm:"unique_index"`
-	Email string `gorm:"unique_index"`
-	Plan  Plan
+	Name      string `gorm:"unique_index"`
+	Email     string `gorm:"unique_index"`
+	Plan      Plan
+	SecretKey SecretKey
 }
 
 //Plans Structure.
 type Plan struct {
-	gorm.Model //Removing gorm.Model leads to duplicate field creation.
-	ClientID   uint
-	Plan       string
+	ID       uint `gorm:"primaryKey"` //Removing gorm.Model leads to duplicate field creation.
+	ClientID uint
+	Plan     string
 }
 
 func (t *Client) TableName() string {
@@ -23,4 +25,18 @@ func (t *Client) TableName() string {
 
 func (t *Plan) TableName() string {
 	return "plans"
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------
+//Structs for api/v1/image
+
+type SecretKey struct {
+	ID        uint `gorm:"primaryKey"`
+	ClientID  uint
+	Accesskey string
+	Secretkey string
+}
+
+func (t *SecretKey) TableName() string {
+	return "secretkey"
 }
