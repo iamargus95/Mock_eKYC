a6b06f628c2d067278d60be7d380a3ae41992c2d
iamargus95 <kamathsuraj95@gmail.com>
2021-10-04 10:27:05 +0530

    added Image func to get header data and validate token

--- golang/eKYC-service-gin/v1/controllers/client_controller.go
+++ golang/eKYC-service-gin/v1/controllers/client_controller.go
@@ -1,12 +1,15 @@
 package v1controller
 
 import (
-	"iamargus95/eKYC-service-gin/middlewares/jwt"
+	"fmt"
+	authtoken "iamargus95/eKYC-service-gin/middlewares/jwt"
 	v1r "iamargus95/eKYC-service-gin/v1/resources"
 	v1s "iamargus95/eKYC-service-gin/v1/services"
 	"net/http"
 	"os"
+	"strings"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
@@ -31,10 +34,44 @@ func Signup(ctx *gin.Context) {
 		return
 	}
 
-	aKey, _ := jwt.GenerateJWT(body.Name)
+	aKey, _ := authtoken.GenerateJWT(body.Name, true)
 	sKey := os.Getenv("MYSIGNINGKEY")
 	ctx.JSON(http.StatusOK, gin.H{
 		"accessKey": aKey,
 		"secretKey": sKey,
 	})
+}
+
+func Image(ctx *gin.Context) {
+
+	auth := ctx.Request.Header.Get("Authorization")
+	if auth == "" {
+		ctx.JSON(http.StatusForbidden, gin.H{
+			"Error": "No Authorization Header found.",
+		})
+		ctx.Abort()
+		return
+	}
+
+	tokenString := strings.TrimPrefix(auth, "Bearer")
+	tokenString = strings.TrimSpace(tokenString)
+	if tokenString == auth {
+		ctx.JSON(http.StatusForbidden, gin.H{
+			"Error": "Could not find bearer token.",
+		})
+		ctx.Abort()
+		return
+	}
+
+	token, err := authtoken.IsValid(tokenString)
+	if err != nil {
+		ctx.JSON(http.StatusForbidden, gin.H{
+			"Error": err.Error(),
+		})
+		ctx.Abort()
+		return
+	}
+
+	claims := token.Claims.(jwt.MapClaims)
+	fmt.Println(claims)
 }
