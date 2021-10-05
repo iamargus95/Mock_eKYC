04086de8d583146d5a9f35de0cf7cd99c2f73296
iamargus95 <kamathsuraj95@gmail.com>
2021-10-05 16:24:11 +0530

    Added a parse claims funcs

--- golang/eKYC-service-gin/middlewares/jwt/token.go
+++ golang/eKYC-service-gin/middlewares/jwt/token.go
@@ -5,29 +5,68 @@ import (
 	"os"
 	"time"
 
-	v1r "iamargus95/eKYC-service-gin/v1/resources"
-
 	"github.com/golang-jwt/jwt"
 )
 
-var mySigningKey = []byte(os.Getenv("MYSIGNINGKEY"))
+type JWTInterface interface {
+	GenerateToken(email string) string
+	ValidateToken(token string) (*jwt.Token, error)
+	ParseToken(token *jwt.Token) (string, error)
+}
 
-func GenerateJWT(user string) (string, error) {
+type authCustomClaims struct {
+	Email string
+	jwt.StandardClaims
+}
 
-	claims := v1r.AuthCustomClaims{
-		user,
+type jwtServices struct {
+	secretKey string
+}
+
+func JWTService() JWTInterface {
+	secret := os.Getenv("MYSIGNINGKEY")
+	return &jwtServices{
+		secretKey: secret,
+	}
+}
+
+func (authtoken *jwtServices) GenerateToken(email string) string {
+	claims := &authCustomClaims{
+		email,
 		jwt.StandardClaims{
 			ExpiresAt: time.Now().Add(time.Hour * 48).Unix(),
 			IssuedAt:  time.Now().Unix(),
 		},
 	}
-
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	//encoded string
-	t, err := token.SignedString(mySigningKey)
+	t, err := token.SignedString([]byte(authtoken.secretKey))
 	if err != nil {
-		return "", fmt.Errorf("invalid signing key %v", err)
+		panic(err)
 	}
-	return t, err
+	return t
+}
+
+func (authtoken *jwtServices) ValidateToken(encodedToken string) (*jwt.Token, error) {
+	return jwt.ParseWithClaims(encodedToken, &authCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, isvalid := token.Method.(*jwt.SigningMethodHMAC); !isvalid {
+			return nil, fmt.Errorf("invalid token: %s", token.Header["alg"])
+
+		}
+		return []byte(authtoken.secretKey), nil
+	})
+}
+
+func (authtoken *jwtServices) ParseToken(token *jwt.Token) (string, error) {
+
+	var email string
+	var err error
+	if claims, ok := token.Claims.(*authCustomClaims); ok && token.Valid {
+		email = fmt.Sprintf(claims.Email)
+	} else {
+		err = fmt.Errorf("token validation failed")
+	}
+
+	return email, err
 }
