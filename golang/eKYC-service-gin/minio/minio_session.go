1a0832c697a1ea72049005847b7f84d0864df668
iamargus95 <kamathsuraj95@gmail.com>
2021-10-06 19:44:27 +0530

    New endpoint

--- golang/eKYC-service-gin/minio/minio_session.go
+++ golang/eKYC-service-gin/minio/minio_session.go
@@ -12,8 +12,8 @@ var minioclient *minio.Client
 func init() {
 
 	endpoint := "172.19.0.3:9090"
-	accessKeyID := "myaccess4minio"
-	secretAccessKey := "mysecret4minio"
+	accessKeyID := "minio123"
+	secretAccessKey := "minio456" //Doubtful
 
 	// Initialize minio client object.
 	newClient, err := minio.New(endpoint, &minio.Options{
