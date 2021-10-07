1478b432c0718b85a3e10033330aabdea70eebc7
iamargus95 <kamathsuraj95@gmail.com>
2021-10-07 21:46:28 +0530

    Added UUID as a tag

--- golang/eKYC-service-gin/minio/store_file.go
+++ golang/eKYC-service-gin/minio/store_file.go
@@ -2,13 +2,16 @@ package minio
 
 import (
 	"context"
+	"fmt"
 	"mime/multipart"
 
+	"github.com/google/uuid"
 	"github.com/minio/minio-go/v7"
 )
 
-func StoreFile(file *multipart.FileHeader) error {
+func StoreFile(client, fileType string, file *multipart.FileHeader) uuid.UUID {
 
+	var Nil uuid.UUID
 	data, _ := file.Open()
 	defer data.Close()
 
@@ -16,8 +19,16 @@ func StoreFile(file *multipart.FileHeader) error {
 	mc := GetMinio()
 
 	size := file.Size
-	_, err := mc.PutObject(context.Background(), "clients", name, data, size,
-		minio.PutObjectOptions{ContentType: "application/octet-stream"})
 
-	return err
+	imageId, _ := uuid.NewUUID()
+	uuid := fmt.Sprintf("%v", imageId) //Convert uuid.UUID to string
+
+	_, err := mc.PutObject(context.Background(), "clients", name, data, size,
+		minio.PutObjectOptions{ContentType: "application/octet-stream", UserTags: map[string]string{"uuid": uuid, "client": client, "type": fileType}}) //Create tags for stored image
+
+	if err != nil {
+		return Nil
+	}
+
+	return imageId
 }
