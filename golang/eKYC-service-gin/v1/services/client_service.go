2f23a47b49f4ecf6cf37f024adb92fdf5617babf
iamargus95 <kamathsuraj95@gmail.com>
2021-10-07 20:51:14 +0530

    Added UUID generation

--- golang/eKYC-service-gin/v1/services/client_service.go
+++ golang/eKYC-service-gin/v1/services/client_service.go
@@ -7,6 +7,8 @@ import (
 	"iamargus95/eKYC-service-gin/v1/models"
 	v1r "iamargus95/eKYC-service-gin/v1/resources"
 	"mime/multipart"
+
+	"github.com/google/uuid"
 )
 
 func Signup(body v1r.SignupPayload) error {
@@ -50,9 +52,11 @@ func Image(name string, file multipart.File, filedata *multipart.FileHeader, fil
 
 	minio.StoreFile(filedata)
 
+	uuid, _ := uuid.NewUUID()
 	newFile = models.FileUpload{
 		ClientID: client.ID,
 		Type:     fileType.Type,
+		UUID:     uuid,
 		Size:     int64(filedata.Size),
 	}
 
