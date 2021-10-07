dee2c2c160b60890b0d058bed2cfe7204cf683de
iamargus95 <kamathsuraj95@gmail.com>
2021-10-07 20:39:00 +0530

    Added tests for /image endpoint [WIP]

--- golang/eKYC-service-gin/v1/controllers/client_controller_test.go
+++ golang/eKYC-service-gin/v1/controllers/client_controller_test.go
@@ -3,8 +3,11 @@ package v1controller_test
 import (
 	"bytes"
 	v1controller "iamargus95/eKYC-service-gin/v1/controllers"
+	"io/ioutil"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -21,7 +24,7 @@ var unauthRequestTests = []struct {
 		url:          "/api/v1/signup",
 		bodyData:     []byte(`{"name": "wangzitian0","email": "testing@one2n.in","plan": "basic"}`),
 		expectedCode: http.StatusOK,
-		responseData: []byte(`{"accessKey": "10-char-JWT-Token","secretKey": "20-char-JWT-Token",}`),
+		responseData: []byte(`{"accessKey": "10-char-JWT-Token"}`),
 	},
 }
 
@@ -46,4 +49,73 @@ func TestSignup(t *testing.T) {
 
 		asserts.Equal(testdata.expectedCode, w.Code)
 	}
+}
+
+func newfileUploadRequest(uri string, params map[string]string, paramName, path string) *http.Request {
+
+	file, err := os.Open(path)
+
+	if err != nil {
+		return nil
+	}
+
+	fileContents, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil
+	}
+
+	fi, err := file.Stat()
+
+	if err != nil {
+		return nil
+	}
+
+	file.Close()
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+
+	part, err := writer.CreateFormFile(paramName, fi.Name())
+
+	if err != nil {
+		return nil
+	}
+
+	part.Write(fileContents)
+
+	for key, val := range params {
+		_ = writer.WriteField(key, val)
+	}
+
+	err = writer.Close()
+	if err != nil {
+		return nil
+	}
+
+	request, _ := http.NewRequest("POST", uri, body)
+	request.Header.Add("Content-Type", writer.FormDataContentType())
+	return request
+}
+
+func TestImage(t *testing.T) {
+
+	filepath, _ := os.Getwd()
+	filepath += "/test.jpeg"
+	asserts := assert.New(t)
+	gin.SetMode(gin.TestMode)
+	r := gin.New()
+
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+
+	extraparams := map[string]string{
+		"type": "face",
+	}
+	c.Request = newfileUploadRequest("http://localhost:8080/api/v1/image", extraparams, "file", filepath)
+	c.Request.Header.Set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJOYW1lIjoic3VyYWoiLCJleHAiOjE2MzM3NDgyMTYsImlhdCI6MTYzMzU3NTQxNn0.PUZ-DQfopIqpN3cZXN2tSvXBnVhwsFIyXu36vfergsO")
+
+	v1controller.Image(c)
+
+	r.ServeHTTP(w, c.Request)
+	asserts.Equal(200, w.Result()) //troubleshooting
+
 }
