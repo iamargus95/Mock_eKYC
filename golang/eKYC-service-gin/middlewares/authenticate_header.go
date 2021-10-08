45dd38be3755fa73c77a401b397cfa9fa7a7df90
iamargus95 <kamathsuraj95@gmail.com>
2021-10-08 20:41:44 +0530

    Changed header authentication to correct 'wrap another handler format'

---
+++ golang/eKYC-service-gin/middlewares/authenticate_header.go
@@ -0,0 +1,54 @@
+package middlewares
+
+import (
+	"net/http"
+	"strings"
+
+	authtoken "iamargus95/eKYC-service-gin/jwt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func EnsureLoggedIn(authtoken authtoken.JWTInterface) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		auth := ctx.Request.Header.Get("Authorization")
+		if auth == "" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"Error": "No Authorization Header found.",
+			})
+			ctx.Abort()
+			return
+		}
+
+		tokenString := strings.TrimPrefix(auth, "Bearer")
+		tokenString = strings.TrimSpace(tokenString)
+		if tokenString == auth {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"Error": "Could not find bearer token.",
+			})
+			ctx.Abort()
+			return
+		}
+
+		token, err := authtoken.ValidateToken(tokenString)
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"Error": err,
+			})
+			ctx.Abort()
+			return
+		}
+
+		name, err := authtoken.ParseToken(token)
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"Error": err,
+			})
+			ctx.Abort()
+			return
+		}
+
+		ctx.Set("client_name", name)
+		ctx.Next()
+	}
+}
