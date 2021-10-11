5792281a67af4a28cf61047d6a5c6ab461d59c42
iamargus95 <kamathsuraj95@gmail.com>
2021-10-11 11:40:44 +0530

    Added edge test cases to signup and image upload

--- golang/eKYC-service-gin/v1/controllers/client_controller_test.go
+++ golang/eKYC-service-gin/v1/controllers/client_controller_test.go
@@ -16,14 +16,38 @@ import (
 )
 
 var unauthRequestTests = []struct {
-	url          string
 	bodyData     []byte
 	expectedCode int
 }{
 	{
-		url:          "/api/v1/signup",
+		// OK
 		bodyData:     []byte(`{"name": "testClient","email": "testing@test.in","plan": "basic"}`),
 		expectedCode: http.StatusOK,
+	},
+	{
+		// Invalid URL
+		bodyData:     []byte(`{"name": "testClient1","email": "testing1@test.in","plan": "basic"}`),
+		expectedCode: http.StatusNotFound,
+	},
+	{
+		// Invalid plan
+		bodyData:     []byte(`{"name": "testClient2","email": "testing2@test.in","plan": "Basic"}`),
+		expectedCode: http.StatusForbidden,
+	},
+	{
+		// Invalid email
+		bodyData:     []byte(`{"name": "testClient3","email": "testing3@in","plan": "basic"}`),
+		expectedCode: http.StatusForbidden,
+	},
+	{
+		// Email not unique
+		bodyData:     []byte(`{"name": "testClient4","email": "testing@test.in","plan": "basic"}`),
+		expectedCode: 400,
+	},
+	{
+		// Name not unique
+		bodyData:     []byte(`{"name": "testClient","email": "testing5@test.in","plan": "basic"}`),
+		expectedCode: 400,
 	},
 }
 
@@ -40,7 +64,7 @@ func TestSignup(t *testing.T) {
 		c, _ := gin.CreateTestContext(w)
 		requestData := testdata.bodyData
 
-		c.Request, _ = http.NewRequest(http.MethodPost, testdata.url, bytes.NewBuffer(requestData))
+		c.Request, _ = http.NewRequest(http.MethodPost, "api/v1/signup", bytes.NewBuffer(requestData))
 		c.Request.Header.Set("Content-Type", "application/json")
 
 		v1controller.Signup(c)
@@ -95,25 +119,55 @@ func newfileUploadRequest(uri string, params map[string]string, paramName, path 
 //Tests for the endpoint /api/v1/image
 func TestImageUpload(t *testing.T) {
 
-	token := authtoken.JWTService().GenerateToken("testClient")
-	filepath, _ := os.Getwd()
-	filepath += "/test.jpeg"
-	asserts := assert.New(t)
-	gin.SetMode(gin.TestMode)
-	r := gin.New()
-
-	w := httptest.NewRecorder()
-	c, _ := gin.CreateTestContext(w)
-
-	extraparams := map[string]string{
-		"type": "face",
+	var testUpload = []struct {
+		filepath     string
+		imageType    string
+		expectedCode int
+	}{
+		{
+			filepath:     "/test.jpeg",
+			imageType:    "face",
+			expectedCode: 200,
+		},
+		{
+			filepath:     "/test.jpeg",
+			imageType:    "id_card",
+			expectedCode: 200,
+		},
+		{
+			filepath:     "/test.jpeg",
+			imageType:    "idcard",
+			expectedCode: 400,
+		},
+		{
+			filepath:     "/test.jpeg",
+			imageType:    "undefined",
+			expectedCode: 400,
+		},
 	}
-	c.Request = newfileUploadRequest("/api/v1/image", extraparams, "file", filepath) //invalid token gen token on the fly
-	c.Request.Header.Set("Authorization", "Bearer "+token)
 
-	v1controller.Image(c)
+	for _, test := range testUpload {
 
-	r.ServeHTTP(w, c.Request)
-	asserts.Equal(200, w.Code)
+		token := authtoken.JWTService().GenerateToken("testClient")
+		filepath, _ := os.Getwd()
+		filepath += test.filepath
+		asserts := assert.New(t)
+		gin.SetMode(gin.TestMode)
+		r := gin.New()
 
+		w := httptest.NewRecorder()
+		c, _ := gin.CreateTestContext(w)
+
+		extraparams := map[string]string{
+			"type": test.imageType,
+		}
+
+		c.Request = newfileUploadRequest("/api/v1/image", extraparams, "file", filepath)
+		c.Request.Header.Set("Authorization", "Bearer "+token)
+
+		v1controller.Image(c)
+
+		r.ServeHTTP(w, c.Request)
+		asserts.Equal(test.expectedCode, w.Code)
+	}
 }
